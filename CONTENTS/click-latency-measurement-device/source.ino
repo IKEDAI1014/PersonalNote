c9d52ac1a2995d24990f2c891c7dad5e3ea0349d
IKEDAI1014 <95849061+IKEDAI1014@users.noreply.github.com>
2023-02-28 22:01:04 +0900

    Update source.ino

--- CONTENTS/click-latency-measurement-device/source.ino
+++ CONTENTS/click-latency-measurement-device/source.ino
@@ -6,9 +6,11 @@ const int PULSE_END_PIN = 4;
 const float MAX_PULSE_DURATION = 100.0;
 const float MIN_PULSE_DURATION = 1.1;
 
+unsigned long previousMillis = 0;
+const long interval = 200;
 unsigned long pulseStart, pulseEnd = 0;
-unsigned int numtries = 0;
-float pulseDuration, avgPulseDuration = 0.0;
+unsigned int numTries = 0;
+float pulseDuration, averagePulseDuration = 0.0;
 
 void setup() {
   Serial.begin(9600);
@@ -19,52 +21,54 @@ void setup() {
 }
 
 void loop() {
-  while (digitalRead(PULSE_START_PIN) != LOW){
-  }
-  pulseStart = micros();
-  delay(1);
-  while (digitalRead(PULSE_END_PIN) != HIGH){
-  }
-  pulseEnd = micros();
-  pulseDuration = (pulseEnd > pulseStart) ? (pulseEnd - pulseStart - 4) / 1000.0 : 0.0;
-  if (pulseDuration < MAX_PULSE_DURATION && pulseDuration > MIN_PULSE_DURATION) {
-    avgPulseDuration = ((avgPulseDuration * numtries) + pulseDuration) / (numtries + 1);
-    numtries += 1;
-    if (numtries == 1) {
-      lcd.home();
-      lcd.print("Try new");
-      lcd.setCursor(8, 0);
-      lcd.print(pulseDuration);
-      lcd.setCursor(14, 0);
-      lcd.print("ms");
-      lcd.setCursor(0, 1);
-      lcd.print(numtries);
-      lcd.setCursor(4, 1);
-      lcd.print("avg");
-      lcd.setCursor(8, 1);
-      lcd.print(avgPulseDuration);
-      lcd.setCursor(14, 1);
-      lcd.print("ms");
-      char buf[7];
-      dtostrf(pulseDuration, 5, 3, buf);
-      Serial.println(buf);
-    } else {
-      lcd.setCursor(8, 0);
-      lcd.print("      ");
-      lcd.setCursor(8, 0);
-      lcd.print(pulseDuration);
-      lcd.setCursor(0, 1);
-      lcd.print("    ");
-      lcd.setCursor(0, 1);
-      lcd.print(numtries);
-      lcd.setCursor(8, 1);
-      lcd.print("      ");
-      lcd.setCursor(8, 1);
-      lcd.print(avgPulseDuration);
-      char buf[7];
-      dtostrf(pulseDuration, 5, 3, buf);
-      Serial.println(buf);
+  if (millis() - previousMillis >= interval) {
+    while (digitalRead(PULSE_START_PIN) != LOW){
+    }
+    delay(1);
+    pulseStart = micros();
+    while (digitalRead(PULSE_END_PIN) != HIGH){
+    }
+    pulseEnd = micros();
+    previousMillis = millis();
+    pulseDuration = ((pulseEnd > pulseStart) ? ((pulseEnd - pulseStart - 4) / 1000.0): 0.0);
+    if (pulseDuration < MAX_PULSE_DURATION && pulseDuration > MIN_PULSE_DURATION) {
+      averagePulseDuration = ((averagePulseDuration * numTries) + pulseDuration) / (numTries + 1);
+      numTries += 1;
+      if (numTries == 1) {
+        lcd.home();
+        lcd.print("Try new");
+        lcd.setCursor(8, 0);
+        lcd.print(pulseDuration);
+        lcd.setCursor(14, 0);
+        lcd.print("ms");
+        lcd.setCursor(0, 1);
+        lcd.print(numTries);
+        lcd.setCursor(4, 1);
+        lcd.print("average");
+        lcd.setCursor(8, 1);
+        lcd.print(averagePulseDuration);
+        lcd.setCursor(14, 1);
+        lcd.print("ms");
+        char buf[7];
+        dtostrf(pulseDuration, 5, 3, buf);
+        Serial.println(buf);
+      } else {
+        lcd.setCursor(8, 0);
+        lcd.print("      ");
+        lcd.setCursor(8, 0);
+        lcd.print(pulseDuration);
+        lcd.setCursor(0, 1);
+        lcd.print("    ");
+        lcd.setCursor(0, 1);
+        lcd.print(numTries);
+        lcd.setCursor(8, 1);
+        lcd.print("      ");
+        lcd.setCursor(8, 1);
+        lcd.print(averagePulseDuration);
+        char buf[7];
+        dtostrf(pulseDuration, 5, 3, buf);
+        Serial.println(buf);
+      }
     }
   }
-  delay(200);
 }
